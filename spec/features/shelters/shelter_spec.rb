022ac224f1803d0b473da8ce551eda1b4f4a9f35
Kevin Krato <kratok08@gmail.com>
2019-11-21 21:33:22 -0700

    Test the ability to create a new shelter

--- spec/features/shelters/shelter_spec.rb
+++ spec/features/shelters/shelter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor" do
-  describe "I visit a shelter show page" do
+  describe "I visit a shelter's show page" do
     it "then I see only the shelter's animal listing" do
       shelter_1 = Shelter.create(name: "RoofsPark", address: "555 Doggo Lane", city: "Denver", state: "Colorado", zip: "55455")
       shelter_2 = Shelter.create(name: "DoogStoor", address: "123 Capital St", city: "Aurora", state: "Colorado", zip: "31331")
@@ -13,6 +13,22 @@ RSpec.describe "As a visitor" do
       expect(page).to have_content(shelter_1.name)
       expect(page).to have_content(shelter_1.address)
       expect(page).to_not have_content(shelter_2.name)
+    end
+  end
+
+  describe "When I visit the new shelter page form by clicking a link on the index" do
+    it "I can create a new shelter" do
+      visit "/shelters"
+
+      click_link "New Shelter"
+
+      expect(current_path).to eq("/shelters/new")
+
+      fill_in "Name", with: "DogsForRent"
+      click_on "Create Shelter"
+
+      expect(current_path).to eq("/shelters")
+      expect(page).to have_content("DogsForRent")
     end
   end
 end
