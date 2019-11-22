71d835256175de0b3765543c994afb4fa6f72efb
Kevin Krato <kratok08@gmail.com>
2019-11-21 18:24:38 -0700

    Add instance methods to find the first animal and average animal age per shelter

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -6,4 +6,12 @@ class Shelter < ApplicationRecord
   def self.shelter_count
     Shelter.count
   end
+
+  def first_animal
+    self.animals.first
+  end
+
+  def avg_animal_age
+    self.animals.average(:age)
+  end
 end
